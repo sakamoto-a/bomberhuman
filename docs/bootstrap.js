7c1deeac499bc47a90f15195c1e974d8878224b8
Yuuki Fujiwara <fujiwara2018@s.okayama-u.ac.jp>
2020-11-25 12:30:09 +0900

    Update demo

--- docs/bootstrap.js
+++ docs/bootstrap.js
@@ -55,6 +55,9 @@
 /******/ 		"../pkg/bomberhuman_bg.wasm": function() {
 /******/ 			return {
 /******/ 				"./bomberhuman_bg.js": {
+/******/ 					"__wbg_playaudio_6dceb6071d2d0a33": function(p0i32,p1i32) {
+/******/ 						return installedModules["../pkg/bomberhuman_bg.js"].exports["__wbg_playaudio_6dceb6071d2d0a33"](p0i32,p1i32);
+/******/ 					},
 /******/ 					"__wbindgen_object_drop_ref": function(p0i32) {
 /******/ 						return installedModules["../pkg/bomberhuman_bg.js"].exports["__wbindgen_object_drop_ref"](p0i32);
 /******/ 					},
@@ -192,7 +195,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/bomberhuman_bg.wasm":"398f83d4603e19c14255"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/bomberhuman_bg.wasm":"1cc61e592042100c20db"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
